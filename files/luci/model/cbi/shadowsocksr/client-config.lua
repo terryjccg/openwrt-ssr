a52012aa0970ce9ccda23b7d4ed1410cdf00d6d3
ywb94 <ywb94@qq.com>
2018-11-17 23:45:53 +0800

    change for openwrt 18.06.1

--- files/luci/model/cbi/shadowsocksr/client-config.lua
+++ files/luci/model/cbi/shadowsocksr/client-config.lua
@@ -20,7 +20,10 @@ end
 
 
 local server_table = {}
-local arp_table = luci.sys.net.arptable() or {}
+
+-- change for openwrt 18.06.1
+--local arp_table = luci.sys.net.arptable() or {}
+local arp_table = luci.ip.neighbors() or {}
 local encrypt_methods = {
 	"table",
 	"rc4",
